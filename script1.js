801b43828f33f875eecce4cb3d4210f610b748ca
khushiaggarwal-06 <khushi0383.be20@chitkara.edu.in>
2022-08-29 12:53:05 +0530

    changes

--- script1.js
+++ script1.js
@@ -12,5 +12,5 @@ window.onload = function () {
     var preloader = document.getElementsByClassName('preloader')[0];
     setTimeout(function () {
       preloader.style.display = 'none';
-    }, 1000);
+    }, 9000);
   };
